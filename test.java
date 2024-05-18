c83760cb45416be188e66d700ad584f8ae5e78f6
LightningFryer <91123163+LightningFryer@users.noreply.github.com>
2024-05-18 12:54:38 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,31 @@
+/**
+ * test
+ */
+
+import java.util.Scanner;
+
+public class test {
+
+    public static void main(String[] args) {
+        int i;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter array limit: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n]; // Declaring the array
+
+        for (i=0; i<n; i++){
+            System.out.print("Enter array element: ");
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.print("\nArray: ");
+
+        for (i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+        // scanner.close();
+    }
+}
